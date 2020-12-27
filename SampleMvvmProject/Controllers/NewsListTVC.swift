85fe408cefa501ae6f9257b75d8f0ccbad1e93b2
Alaa elrhman <alaaelrhmanarafa@gmail.com>
2020-12-27 08:49:01 +0200

    Add ArticleViewModel and ArticlesListViewModel

--- SampleMvvmProject/Controllers/NewsListTVC.swift
+++ SampleMvvmProject/Controllers/NewsListTVC.swift
@@ -11,9 +11,17 @@ class NewsListTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setup()
     }
 
+    private func setup(){
+        if let url = URL(string: "https:newsapi.org/v2/everything?q=bitcoin&from=2020-11-05&sortBy=publishedAt&apiKey=9f72bbe87d03436b92f0bff59534d5a0"){
+            WebServices().getArticals(url: url) { (_) in
+                
+            }
+        }
+//        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
